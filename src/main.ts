f2a0736dc8a774c8eaccbb81a1ec330bc0c389a3
Cameron Romo <cromo1225@gmail.com>
2021-07-23 17:25:18 -0600

    Roll back changes to access the project folders

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
-import { LikeComponent } from './app/like.component';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-let component = new LikeComponent(10, true);
-component.onClick();
-console.log(`likesCount: ${component.likesCount}, isSelected: ${component.isSelected}`);
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
