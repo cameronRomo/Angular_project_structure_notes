c1e6955acdf3214c3f05d9c08e9e58981569ec3f
Cameron Romo <cromo1225@gmail.com>
2021-07-26 17:29:40 -0600

    Add post object as an input property and define a onFavoriteChanged method to log output data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+import { FavoriteChangedEventArgs } from './favorite/favorite.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  post: any = {
+    title: "Title",
+    isFavorite: false,
+  };
   title: string = '';
+
+  onFavoriteChanged(eventArgs: FavoriteChangedEventArgs) {
+    console.log(eventArgs);
+  }
 }
