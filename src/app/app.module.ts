1a5a2357ae3fb728c6da2741ca6486cc93414cef
Cameron Romo <cromo1225@gmail.com>
2021-07-26 15:39:13 -0600

    Add FormsModule and cusom pipe to the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthorsComponent } from './authors/authors.component';
 import { FavoriteComponent } from './favorite/favorite.component';
+import { TitleCasePipe } from './title-case.pipe';
 // import { AuthorsService } from './authors.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthorsComponent,
-    FavoriteComponent
+    FavoriteComponent,
+    TitleCasePipe
   ],
   imports: [
   BrowserModule,
     AppRoutingModule,
+  FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
