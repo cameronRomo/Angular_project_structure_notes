fde2f8baf0295901a2c0d06038d1080e55ab48d0
Cameron Romo <cromo1225@gmail.com>
2021-07-26 15:16:32 -0600

    Initial implementation for checking prepositions

--- src/app/title-case.pipe.ts
+++ src/app/title-case.pipe.ts
@@ -5,8 +5,25 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TitleCasePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string): any {
+    if (!value) return null;
+
+    let prepositions = [
+      'of',
+      'the'
+    ];
+
+    let words = value.split(' ');
+
+    for (let i = 0; i < words.length; i++) {
+      if (i > 0 && prepositions.includes(words[i].toLowerCase())) 
+        words[i] = words[i].toLocaleLowerCase();
+      else {
+        words[i] = words[i].substring(0, 1).toUpperCase() + words[i].substr(1).toLowerCase();
+      }
+    }
+
+    return words.join(' ');
   }
 
 }
