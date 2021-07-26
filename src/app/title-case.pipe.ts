ea0596a31716df2113761a6a336ac230415df8b4
Cameron Romo <cromo1225@gmail.com>
2021-07-26 15:36:30 -0600

    Add pipe functionality to detect prepositions under conditions to make them either capital or lowercase

--- src/app/title-case.pipe.ts
+++ src/app/title-case.pipe.ts
@@ -8,22 +8,31 @@ export class TitleCasePipe implements PipeTransform {
   transform(value: string): any {
     if (!value) return null;
 
-    let prepositions = [
-      'of',
-      'the'
-    ];
-
     let words = value.split(' ');
 
     for (let i = 0; i < words.length; i++) {
-      if (i > 0 && prepositions.includes(words[i].toLowerCase())) 
-        words[i] = words[i].toLocaleLowerCase();
-      else {
-        words[i] = words[i].substring(0, 1).toUpperCase() + words[i].substr(1).toLowerCase();
-      }
+      let word = words[i];
+
+      if (i > 0 && this.isPreposition(word)) 
+        words[i] = word.toLowerCase();
+      else
+        words[i] = this.toTitleCase(word);
     }
 
     return words.join(' ');
+  }
+
+  private toTitleCase(word: string): string {
+    return word.substring(0, 1).toUpperCase() + word.substr(1).toLowerCase();
+  }
+
+  private isPreposition(word: string): boolean {
+    let prepositions = [
+    'of',
+    'the'
+  ];
+  
+    return prepositions.includes(word.toLowerCase());
   }
 
 }
