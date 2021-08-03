e478d31f9a6018953999f7afae961007317d79be
Cameron Romo <cromo1225@gmail.com>
2021-08-03 17:11:45 -0600

    Add HostListener to have acess to its DOM element

---
+++ src/app/input-format.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appInputFormat]'
+})
+export class InputFormatDirective {
+  @HostListener('focus') onFocus() {
+    console.log("on focus!");
+    
+  }
+
+  @HostListener('blur') onBlur() {
+    console.log("on blur!");
+    
+  }
+
+  constructor() { }
+
+}
