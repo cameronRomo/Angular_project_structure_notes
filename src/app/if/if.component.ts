2f93d4528acec86e2a90ff143efa269e5791c4b5
Cameron Romo <cromo1225@gmail.com>
2021-08-03 15:08:04 -0600

    Add if component data to show how the ngIf directive works

---
+++ src/app/if/if.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'if',
+  templateUrl: './if.component.html',
+  styleUrls: ['./if.component.css']
+})
+export class IfComponent{
+  courses = [1, 2];
+}
