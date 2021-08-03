716271a0f9f50a6638e8e0d8e900bb45a7b9726f
Cameron Romo <cromo1225@gmail.com>
2021-08-03 17:35:31 -0600

    Add input to help format onBlur to bring a string to all lowercase

--- src/app/input-format.directive.ts
+++ src/app/input-format.directive.ts
@@ -1,19 +1,17 @@
-import { Directive, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appInputFormat]'
 })
 export class InputFormatDirective {
-  @HostListener('focus') onFocus() {
-    console.log("on focus!");
-    
-  }
+  @Input('appInputFormat') format: any;
+  constructor(private el: ElementRef) { }
 
   @HostListener('blur') onBlur() {
-    console.log("on blur!");
-    
+    let value: string = this.el.nativeElement.value;
+    if (this.format == 'lowercase')
+      this.el.nativeElement.value = value.toLowerCase();
+    else
+      this.el.nativeElement.value = value.toUpperCase();
   }
-
-  constructor() { }
-
 }
